e313d50188c3e1b03a0a1f368eea47fdce3d6494
Hardik Shah <hhshah032@gmail.com>
2025-10-30 17:57:48 +0530

    Update Q001.py

--- Python/Q001.py
+++ Python/Q001.py
@@ -1,3 +1,40 @@
 # a = ["M","na","i","par", "xyz"]
 # b = ["y","me","s","th"]
 # output = ['My', 'name', 'is', 'parth', 'xyz']
+
+
+a = ["M", "na", "i", "par", "xyz"]
+b = ["y", "me", "s", "th"]
+
+## create result empty list and append the a[i] and b[i[
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+result = []
+for i in range(len(a)):
+  if i < len(b):
+    result.append(a[i] + b[i])
+  else:
+    result.append(a[i])
+
+print(result)
